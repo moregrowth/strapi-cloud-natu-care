dc0fcd76cdc00192358c52851a8903dc18253f31
John Paul Web <web@moregrowth.com>
2024-07-07 21:06:26 +0200

    update plugins

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -24,7 +24,7 @@ module.exports = ({ env }) => ({
                  * It is useful because strapi sets folder names with numbers, not by user's input folder name
                  * By default it is false
                  */
-                pool: true
+                pool: false
             },
             actionOptions: {
                 upload: {},
