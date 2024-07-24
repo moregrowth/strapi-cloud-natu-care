47519d7038b8ce3ca08aec8fcfe1a49f4f041829
John Paul Web <web@moregrowth.com>
2024-07-24 23:06:32 +0200

    update tinymce

--- src/plugins/wysiwyg/admin/src/components/Tinymce/index.js
+++ src/plugins/wysiwyg/admin/src/components/Tinymce/index.js
@@ -16,14 +16,10 @@ const TinyEditor = ({ onChange, name, value }) => {
       outputFormat="html"
       init={{
         plugins: [
-          'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-          'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-          'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+          'advlist image code emoticons link lists table'
         ],
         toolbar: 'undo redo | blocks | ' +
-          'bold italic forecolor | alignleft aligncenter ' +
-          'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
+          'bold image code italic | bullist numlist | link emoticons',
         content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
       }}
     />
